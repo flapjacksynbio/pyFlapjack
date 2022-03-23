a15d93e3c1a604692f9d554ab14aa99f1e5a9936
Gonzalo Vidal <35148159+Gonza10V@users.noreply.github.com>
2022-03-23 02:34:49 -0300

    adding autoapi call

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,21 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = [
+    'autoapi.extension',
 ]
+
+# See https://sphinx-autoapi.readthedocs.io/en/latest/reference/config.html
+autoapi_dirs = ['../flapjack']
+# autoapi_options = ['members', 'undoc-members', 'show-inheritance',
+#                   'show-module-summary', 'special-members']
+autoapi_type = 'python'
+autoapi_python_class_content = 'both'
+autoapi_member_order = 'alphabetical'
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -42,12 +55,15 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
